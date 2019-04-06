de4fcf97207762a95ff4faff8d7a2c8b1b91e86f
Toma Morisawa <13.35.104@gmail.com>
2019-04-06 13:32:52 +0900

    update files
    to use view functions

--- hello.py
+++ hello.py
@@ -1,12 +1,47 @@
-from flask import Flask
+from flask import Flask, redirect
+from werkzeug.routing import RequestRedirect, MethodNotAllowed, NotFound
+
 app = Flask(__name__)
+
+def get_view_function(url, method='GET'):
+    """Match a url and return the view and arguments
+    it will be called with, or None if there is no view.
+    """
+
+    adapter = app.url_map.bind('localhost')
+
+    try:
+        match = adapter.match(url, method=method)
+    except RequestRedirect as e:
+        # recursively match redirects
+        return get_view_function(e.new_url, method)
+    except (MethodNotAllowed, NotFound):
+        # no match
+        return None
+
+    try:
+        # return the view function and arguments
+        return app.view_functions[match[0]], match[1]
+    except KeyError:
+        # no view is associated with the endpoint
+        return None
+
+
+def hello_world(request):
+    # return redirect()
+    return get_view_function(request['path'])[0]()
 
 
 @app.route('/')
-def hello_world(request):
-    return f'Hello World!'
+def index():
+    return f'here is index'
 
 
 @app.route('/hoge')
-def hoge(request):
+def hoge():
     return f'hoge!'
+
+
+if __name__ == '__main__':
+    request = {'path': 'hoge'}
+    print(hello_world(request))
