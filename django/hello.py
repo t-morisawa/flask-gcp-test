66f7ff8ed8721b70d2f560abc2ee8732656779d3
Toma Morisawa <13.35.104@gmail.com>
2019-04-08 21:11:16 +0900

    update for python3

--- django/hello.py
+++ django/hello.py
@@ -1,39 +1,30 @@
-# -*- coding:utf-8 -*-
-# hello.py
-
 import sys
 from django.conf.urls import url
-from django.http import HttpResponse
-
-
-def index(request):
-    import random
-    return HttpResponse('Hello World. random={}\n'.format(random.random()))
-
+# from django.core.urlresolvers import resolve # for django 1
+from django.urls import reverse
+from django.conf import settings
 
 def hoge(request):
-    import random
-    return HttpResponse('Hello Hoge World. random={}\n'.format(random.random()))
+    return 'here is hoge'
 
+def index(request):
+    return 'here is index'
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^hoge$', hoge),
+    url(r'hoge$', hoge),
 ]
 
-if __name__ == "__main__":
-    from django.core import management
-    from django.conf import settings
-
-    settings.configure(
-        ALLOWED_HOSTS=["*"],
-        DEBUG=True,
-        ROOT_URLCONF=__name__,
-        MIDDLEWARE_CLASSES=(
-            'django.middleware.common.CommonMiddleware',
+def handler(request):
+    if not settings.configured:
+        settings.configure(
+            ALLOWED_HOSTS=["*"],
+            DEBUG=True,
+            ROOT_URLCONF=__name__,
+            MIDDLEWARE_CLASSES=(
+                'django.middleware.common.CommonMiddleware',
+            )
         )
-    )
-    # using this via 'python server-example.py runserver'
-    management.execute_from_command_line(sys.argv)
 
-    # management.call_command('runserver')
+    return reverse('index')
+    # return resolve('index')
